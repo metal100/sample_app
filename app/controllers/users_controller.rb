0971ebdb20abfc2cfa53597111a5417ca8b4bb06
takumi sato <takumetal@outlook.jp>
2017-02-14 17:27:44 +0900

    Finish log in/log out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-  
+
   def show
     @user = User.find(params[:id])
   end
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
    @user = User.new(user_params)
    if @user.save
      # Handle a successful save.
+     log_in @user
      flash[:success] = "Welcome to the Sample App!"
      redirect_to @user
    else

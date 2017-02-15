e9bef1f2bcd691fa5d7c3d17458cc4c3fdf822f3
takumi sato <takumetal@outlook.jp>
2017-02-15 16:22:10 +0900

    Add account activations

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -24,16 +24,14 @@ module SessionsHelper
   end
 
   # 現在ログインしているユーザーを返す (ユーザーがログイン中の場合のみ)
- def current_user
-      if (user_id = session[:user_id])
-     @current_user ||= User.find_by(id: session[:user_id])
-   elsif (user_id = cookies.signed[:user_id])
-     raise       # テストがパスすれば、この部分がテストされていないことがわかる
-     user = User.find_by(id: user_id)
-     if user && user.authenticated?(cookies[:remember_token])
-       log_in user
-       @current_user = user
-
+  def current_user
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(:remember, cookies[:remember_token])
+        log_in user
+        @current_user = user
      end
    end
  end
